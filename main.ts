5ec39a961fb2173ebf3d7f1a4af02ed0508b94ea
Marta-Nora <130647476+Marta-Nora@users.noreply.github.com>
2023-06-05 08:44:00 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(humedad)
-})
-let humedad = 0
-humedad = radio.receivedPacket(RadioPacketProperty.SignalStrength)
